4569fe5dcc428d72670a02bf9916cab53b849f49
Sam-Web-Tech <oriolasamuel19@gmail.com>
2026-05-07 05:23:50 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -9,6 +9,7 @@ import Footer from "./Footer";
 import MainContent from "./Main";
 import MyMiddle from "./Middle";
 import LastPage from "./Last";
+import Home from "./Home";
 
 import Rent from "./pages/Rent";
 import Lease from "./pages/Lease";
@@ -22,9 +23,21 @@ function App() {
       <Mynav />
 
       <Routes>
-
+        {/* Redirect empty route to home content */}
         <Route
           path="/"
+          element={
+            <>
+              <MainContent />
+              <MyMiddle />
+              <LastPage />
+            </>
+          }
+        />
+
+        {/* Optional: also make /home go to same main page */}
+        <Route
+          path="/home"
           element={
             <>
               <MainContent />
@@ -37,11 +50,11 @@ function App() {
         <Route path="/lease" element={<Lease />} />
         <Route path="/rent" element={<Rent />} />
         <Route path="/sales" element={<Sales />} />
-
         <Route path="/contact" element={<Contact />} />
-
         <Route path="/fillform" element={<FillForm />} />
 
+        {/* Redirect unknown routes → home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
 
       <Footer />
