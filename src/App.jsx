deb2fc5a2525a1bb2337706821b443894c87c750
Sam-Web-Tech <oriolasamuel19@gmail.com>
2026-05-07 15:35:46 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -9,7 +10,6 @@ import Footer from "./Footer";
 import MainContent from "./Main";
 import MyMiddle from "./Middle";
 import LastPage from "./Last";
-import Home from "./Home";
 
 import Rent from "./pages/Rent";
 import Lease from "./pages/Lease";
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,5 @@
 import properties from "./Data";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function MainContent() {
  
